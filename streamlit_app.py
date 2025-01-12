2489e4687d064fcda09cc4aa3935babbc69cbca4
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-12 18:26:47 +0000

    NyCommits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st  
 
-st.write(""" Hei på deg""")
+#st.write(""" Hei på deg""")
 
-#from streamlit_qrcode_scanner import qrcode_scanner  
+from streamlit_qrcode_scanner import qrcode_scanner  
 
-#qr_code = qrcode_scanner(key='qrcode_scanner')  
+qr_code = qrcode_scanner(key='qrcode_scanner')  
 
-#if qr_code:  
-#  st.write(qr_code) 
+if qr_code:
+  st.write(qr_code) 
