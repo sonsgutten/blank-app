1ea35fda3d97afa446d78bc3cd8d9c48a21cef97
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-21 20:56:14 +0000

    writeqr

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,10 @@ from pymongo.mongo_client import MongoClient
 qr_code = qrcode_scanner(key='qrcode_scanner')  
 client = MongoClient(st.secrets["uri"])
 
-
-try:
-    client.admin.command('ping')
-    st.write("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    st.write(e)
-
-
-
 if qr_code:
   db = client.MyData
   collection = db['MyData']
-  st.write(qr_code)
   new_data={'qr':qr_code}
   collection.insert_one(new_data) 
+  st.write(qr_code)
 
