bc236191989429ef554aef56a64cd5604ba45685
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-21 21:16:48 +0000

    write

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,13 @@ from streamlit_qrcode_scanner import qrcode_scanner
 from pymongo.mongo_client import MongoClient
 
 qr_code = qrcode_scanner(key='qrcode_scanner')  
-client = MongoClient(st.secrets["uri"])
+
 
 if qr_code:
+  client = MongoClient(st.secrets["uri"])
   db = client.MyData
   collection = db['MyData']
-  new_data = {'desc': 'desc',
- 'name': 'username',
- 'qr':qr_code}
+  new_data = {'desc': 'desc','name': 'username','qr':qr_code}
   collection.insert_one(new_data) 
   st.write(qr_code)
 
