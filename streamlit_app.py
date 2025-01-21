9db77594eb7e0b54f71c4d591199815bfe837da6
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-21 20:59:30 +0000

    write

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,9 @@ client = MongoClient(st.secrets["uri"])
 if qr_code:
   db = client.MyData
   collection = db['MyData']
-  new_data={'qr':qr_code}
+  new_data = {'desc': 'desc',
+ 'name': 'username',
+ 'qr':qr_code}
   collection.insert_one(new_data) 
   st.write(qr_code)
 
