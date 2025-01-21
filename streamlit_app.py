172bc2dab7e5791671394a0632ba9bc65ddda898
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-21 21:21:57 +0000

    med button

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,11 @@ qr_code = qrcode_scanner(key='qrcode_scanner')
 
 
 if qr_code:
+  st.write(qr_code)
+
+if st.button('UpdateDB'):
   client = MongoClient(st.secrets["uri"])
   db = client.MyData
   collection = db['MyData']
   new_data = {'desc': 'desc','name': 'username','qr':qr_code}
   collection.insert_one(new_data) 
-  st.write(qr_code)
-
