35ea2b76ee5eb0fef25ea235933ac0da176c6922
Marcus Nordahl <sonsgutten@gmail.com>
2025-01-21 20:39:48 +0000

    Skal skrive qr til db

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st  
-from streamlit_qrcode_scanner import qrcode_scanner  
+from streamlit_qrcode_scanner import qrcode_scanner
+from pymongo.mongo_client import MongoClient
 
 qr_code = qrcode_scanner(key='qrcode_scanner')  
+client = MongoClient(st.secrets["uri"])
+db = client.MyData
+collection = db['MyData']
 
 
 if qr_code:
-  st.write(qr_code) 
+  st.write(qr_code)
+  new_data={'qr':qr_code}
+  collection.insert_one(new_data) 
 
-
-#st.write(f"{item['name']} has a :{item['pet']}:")
-st.write(st.secrets["uri"])
